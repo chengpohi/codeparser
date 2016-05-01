63b6e8d0bfa967eef8859994937b4fd0d1e4b92f
Xiang Chen <chengpohi@gmail.com>
2016-05-01 23:31:03 +0800

    DONE:basic---TODO:

---
+++ src/main/scala/com/github/chengpohi/parser/Basic.scala
@@ -0,0 +1,35 @@
+package com.github.chengpohi.parser
+
+import fastparse.all._
+
+/**
+  * codeparser
+  * Created by chengpohi on 5/1/16.
+  */
+object Basic {
+  val UnicodeEscape = P("u" ~ HexDigit ~ HexDigit ~ HexDigit ~ HexDigit)
+
+  val digits = "0123456789"
+  val Digit = P(CharIn(digits))
+  val hexDigits = digits + "abcdefABCDEF"
+  val HexDigit = P(CharIn(hexDigits))
+  val HexNum = P("0x" ~ CharsWhile(hexDigits.contains(_)))
+  val DecNum = P(CharsWhile(digits.contains(_)))
+  val Exp = P(CharIn("Ee") ~ CharIn("+-").? ~ DecNum)
+  val FloatType = P(CharIn("fFdD"))
+
+  val WSChars = P(CharsWhile("\u0020\u0009".contains(_)))
+  val Newline = P(StringIn("\r\n", "\n"))
+  val Semi = P(";" | Newline.rep(1))
+  val OpChar = P(CharPred(isOpChar))
+
+  def isOpChar(c: Char) = {
+    fastparse.CharPredicates.isOtherSymbol(c) || fastparse.CharPredicates.isMathSymbol(c) || "!%&*+-/:<=>?@\\^|~".contains(c)
+  }
+
+  val Letter = P(CharPred(c => c.isLetter | c.isDigit))
+  val LetterDigitDollarUnderscore = P(CharPred(c => c.isLetter | c.isDigit))
+  val Lower = P(CharPred(c => c.isLower))
+  val Upper = P(CharPred(_.isUpper))
+}
+
